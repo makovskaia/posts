b9a15879159228c1098e6d5487d2bde434ef56b6
mawa <makovska.mariia@bws.pp.ua>
2024-12-02 09:55:00 +0200

    styling: same cards height and display fixed

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,8 @@
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
@@ -16,16 +18,7 @@ type PostProps = {
 	onDeletePost: (id: number)=>{},
 
 }
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
-  }),
-}));
+
 
 const cardActionStyle = {
   alignSelf: "stretch",
@@ -33,12 +26,19 @@ const cardActionStyle = {
   justifyContent: "flex-end",
   alignItems: "flex-start",
   p: 0
+}
 
+const cardStyle={
+	height: '100%',
+	display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    padding: '0.4em'
 }
 
 export const Post = ({ id, title, body, onEditPost, onDeletePost }: PostProps) => (
-	<Item>
-		<CardContent>	
+	<Card sx={cardStyle}>
+		<CardContent>
 			<Typography variant="h6" gutterBottom>{title}</Typography>
 			<Typography variant="body2" gutterBottom>{body}</Typography>
 		</CardContent>
@@ -46,5 +46,5 @@ export const Post = ({ id, title, body, onEditPost, onDeletePost }: PostProps) =
 			<Button size="small" onClick={()=>{onEditPost({ id, title, body, userId: 11 })}}><EditIcon/></Button>
 			<Button size="small" onClick={()=>{onDeletePost(id)}}><DeleteIcon/></Button>
 		</CardActions>
-	</Item>
+	</Card>
 )
